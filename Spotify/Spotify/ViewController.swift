de395a25edad713053450053ca5b353dfbe0e1d7
Romain Mullot <romain.mullot@lilo.org>
2019-01-24 16:24:54 +0100

    Added Project Configurated infoplist

---
+++ Spotify/Spotify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Romain Mullot on 24/01/2019.
+//  Copyright © 2019 Romain Mullot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
